a9363536987d51c16fa6f2f99f8ae3fafd8cfa76
Andreas Jung <yet@gmx.de>
2024-05-20 20:44:43 -0400

    updated

--- fastapi_auth/jinja2_templates.py
+++ fastapi_auth/jinja2_templates.py
@@ -1,10 +1,7 @@
 """Jinja2 templates for FastAPI."""
 
-
-
 from fastapi.templating import Jinja2Templates
 from jinja2 import Environment, PackageLoader
-
 
 
 env = Environment(loader=PackageLoader("fastapi_auth", "templates"))
