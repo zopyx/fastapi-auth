e42ef2361c0a47bd81d3303e48ce811f4b12dd3b
Andreas Jung <info@zopyx.com>
2024-05-23 12:29:39 +0200

    updated

--- fastapi_auth/tests/test_user_management_sqlobject.py
+++ fastapi_auth/tests/test_user_management_sqlobject.py
@@ -2,6 +2,7 @@ import unittest
 from ..user_management_sqlobject import UserManagement
 
 import os
+import pytest
 
 
 class TestUserManagement(unittest.TestCase):
@@ -15,16 +16,28 @@ class TestUserManagement(unittest.TestCase):
     def test_add_user(self):
         self.um.add_user("admin", "password", "admin,user")
         self.assertTrue(self.um.has_user("admin"))
+    def test_add_user_twice(self):
+        self.um.add_user("admin", "password", "admin,user")
+        with pytest.raises(ValueError):
+            self.um.add_user("admin", "password", "admin,user")
 
     def test_delete_user(self):
         self.um.add_user("admin", "password", "admin,user")
         self.um.delete_user("admin")
         self.assertFalse(self.um.has_user("admin"))
 
+    def test_delete_non_existing_user(self):
+        with pytest.raises(ValueError):
+            self.um.delete_user("admin")
+
     def test_get_user(self):
         self.um.add_user("admin", "password", "admin,user")
         user = self.um.get_user("admin", "password")
         self.assertEqual(user, {"username": "admin", "roles": ["admin", "user"]})
+
+    def test_get_non_existing_user(self):
+        user = self.um.get_user("admin", "password")
+        self.assertIsNone(user)
 
     def test_get_users(self):
         self.um.add_user("admin", "password", "admin,user")
@@ -41,10 +54,14 @@ class TestUserManagement(unittest.TestCase):
         user = self.um.get_user("admin", "new_password")
         self.assertEqual(user, {"username": "admin", "roles": ["admin", "user"]})
 
+    def test_change_password_non_existing_user(self):
+        with pytest.raises(ValueError):
+            self.um.change_password("admin", "new_password")
+
     def test_verify_password(self):
         self.um.add_user("admin", "password", "admin,user")
         self.assertTrue(self.um.verify_password("admin", "password"))
 
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_verify_password_non_existing_user(self):
+        with pytest.raises(ValueError):
+            self.um.verify_password("admin", "password")
